4bd4fe19fe4a7398e96c2015f8a2a706ed25112d
orso081980 <orsacchiotto80@hotmail.com>
2015-04-21 21:27:22 -0400

    fuck

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.use('/uploads', uploads)
 
 // db 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://orsacchiotto80@hotmail.com:18081980Mr@ds061751.mongolab.com:61751/heroku_app35620707');
+mongoose.connect('mongodb://heroku_app35620707:18081980Mr@ds061751.mongolab.com:61751/heroku_app35620707');
 
 
 // tell the application which route / controller to use for product requests
